a3b7b264d0738502bbc3dac77743b9c3d8ace760
Om Shah <ojsjvs123@gmail.com>
2025-02-17 10:10:03 +0530

    Day 8 files

---
+++ day8/d8s1.py
@@ -0,0 +1,41 @@
+# List
+
+fruits = ["Apple", ["Mango", "Banana"], "Papaya", "Watermelon", 1, 99.99, True]
+
+#           0              1               2         3          4    5      6
+#          -7             -6             -5       -4        -3    -2       -1
+
+# print(fruits[-6][0]) # ["Mango", "Banana"]
+
+
+#print(fruits[::-2])
+
+marks = [55, 65, 30, 70, 90, 45.5, 60.5]
+
+print(marks)
+
+'''
+marks.sort()
+
+print(marks)
+print(marks)
+print(marks)
+print(marks)
+'''
+
+
+
+# marks.sort()
+# print(marks)
+
+# marks.sort() is the same as marks = sorted(marks)
+
+print(sorted(marks))
+
+print(marks)
+print(marks)
+print(marks)
+print(marks)
+
+
+
