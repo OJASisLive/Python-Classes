60d803908187ef1e68f54e51155dac68952cbd54
Om Shah <ojsjvs123@gmail.com>
2025-04-01 19:49:11 +0530

    Day 21 had nothing + day 22

---
+++ day22/day22.py
@@ -0,0 +1,35 @@
+#Looping - Repeating something
+# 1. While loop (repeat until a condition remains satisfied)
+# 2. For loop (repeat for 'n' number of times)
+
+
+a = 10
+while a>0:
+    print(a)
+    a = a-1 #Iteration
+print(a, "is zero now")
+
+'''
+10   #a = 10-1 = 9
+        9>0 so
+9    #a = 9-1 = 8
+            8>0
+8    # 7
+            7>0
+7
+            6>0
+6
+            5>0
+5
+            4>0
+4
+            3>0
+3
+            2>0
+2
+            1>0
+1    #1-1 = 0
+            0 not greater than 0
+            exit loop
+0 is zero now
+'''
---
+++ day22/while_loop_5_table.py
@@ -0,0 +1,72 @@
+# 5 table program
+
+'''
+print(5*1)
+print(5*2)
+print(5*3)
+print(5*4)
+print(5*5)
+'''
+
+# Step 1: Identify what remains the same in each statement
+'''
+a = 1
+while a<=5:
+    # Iteration
+    print("5","*",a,"=",5*a)
+    a = a+1
+print("I entered the else block")
+'''
+# Fibonacci series
+#0,1,1,2,3,5,8,13,21,34
+
+#next number = current + previous
+
+#0,1,1,2,3,
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+prev = 0
+curr = 1
+print(prev)
+print(curr)
+nex = 0
+while nex<100:
+    nex = prev+curr
+    print(nex)
+    prev = curr
+    curr = nex
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
