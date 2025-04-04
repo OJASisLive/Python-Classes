23a71b0c7f0aac40849b71f9fd15488fb936cc8e
Om Shah <ojsjvs123@gmail.com>
2025-04-04 19:48:32 +0530

    day24

---
+++ day24/infinite.py
@@ -0,0 +1,64 @@
+# Infinite loop, break and continue statements
+
+# pass means "Do Nothing"
+
+# break exits out of the loop
+
+while True:
+    print("Hello")
+    a = input("Should I continue?(y/n) : ")
+    a = a.upper()
+    if a == 'N':
+        break
+
+
+
+    
+'''
+Iteration 1:
+a=0;   0<5 = True; a+=1 = 1
+Iteration 2:
+a = 1; 1<5 = True; while True a+=1 = 2
+IT 3:
+a = 2; 2<5 = True; while True a+=1 = 3
+IT 4:
+a = 3; 3<5 = True; while True a+=1 = 4
+IT 5:
+a = 4; 4<5 = True; while True a+=1 = 5
+IT 6:
+a = 5; 5<5 = False; while False: 
+'''
+
+
+
+
+
+'''
+
+Ask :
+    Are you a fool?(y/n)
+    n
+    Are you a fool?
+    n
+    Are you a fool?
+    y
+    I knew it!
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
